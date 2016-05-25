63d2b7e97d913e0230004150212d060280f5f5be
kurui <kurui@mogujie.com>
2016-05-25 18:34:49 +0800

    数据处理完毕

--- app/Http/Controllers/MakeProductsController.php
+++ app/Http/Controllers/MakeProductsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -12,9 +14,8 @@ class MakeProductsController extends Controller
     //
     public function make(){
 
-        $baseUrl = "photos.pearson.com/";
         for($i=1; $i<1001; $i++) {
-            $url = "photos.pearson.com/$i.jpg";
+            $url = "http://photos.pearson.com/$i.jpg";
 
             $product = new Product();
             $product->id = $i;
@@ -23,6 +24,20 @@ class MakeProductsController extends Controller
             $product->detail = "Book$i :This is a good book, this book is willing to sale";
 
             $product->save();
+        }
+    }
+
+    public function makeUser(){
+        for ($i=1; $i<=200; $i++){
+            $user = User::find($i);
+
+            $user->name = "user$i";
+            $user->password = "123";
+            $user->email = "user$i@163.com";
+            $user->created_at = Carbon::now();
+            $user->updated_at = Carbon::now();
+
+            $user->save();
         }
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,7 @@ Route::get('/showProductsPage', 'ShowProductsController@queryProductsPage');
 Route::get('/recommend_products', 'RecommendProductsController@index');
 Route::get('/product/{id}', 'ProductDetailController@show');
 Route::get('/makeproducts', 'MakeProductsController@make');
+Route::get('/makeusers', 'MakeProductsController@makeUser');
 
 Route::post('/register/create', 'UserRegisterController@create');
 Route::post('/login', 'UserLoginController@index');
