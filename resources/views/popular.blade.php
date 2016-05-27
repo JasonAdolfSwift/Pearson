840fa1242c4472eb4e44fe1a4706c9a1813ecfae
kurui <kurui@mogujie.com>
2016-05-27 23:17:02 +0800

    输出排名

--- resources/views/popular.blade.php
+++ resources/views/popular.blade.php
@@ -4,7 +4,10 @@
         <h2 align="center">最受欢迎图书</h2>
         <hr/>
         <div style="width:90%; margin: 5px 500px">
-            @foreach($popular as $product)
+            @foreach($popular as $key=>$product)
+                    <div style="width:30%; text-align:center; font-size: 15px;font-weight: 600">
+                        第{{$key}}名
+                    </div>
                     <a href="/product/{{$product->id}}">
                         <img style="width:30%;height:300px;display: inline-block;margin:5px 5px;" src="{{$product->photo_path}}">
                     </a>
